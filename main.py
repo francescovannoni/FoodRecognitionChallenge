0b781ac6eb7dc914cd26476dd1555b1ac53bca0a
frenk <francesco.vannoni@stud.unifi.it>
2021-09-03 10:54:06 +0200

    import libraries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import cv2
 import pandas as pd
 import numpy as np
+import platform
 
 if not os.path.exists("data"):
     !mkdir data
@@ -18,3 +19,5 @@ if not os.path.exists("data/train/images"):
 if not os.path.exists("data/val/images"):
     !wget https://datasets.aicrowd.com/default/aicrowd-public-datasets/food-recognition-challenge/v0.4/val-v0.4.tar.gz
     !unzip -q val-v0.4.tar.gz -d ./data
+
+
