0d1932b6d68063e9fa43a5fe93a6eb96db9a0b91
frenk <francesco.vannoni@stud.unifi.it>
2021-09-04 13:02:01 +0200

    preprocessing

--- main.py
+++ main.py
@@ -1,90 +1,101 @@
 # Downloading data (and exploring)
 
 import os
-import wget
+import numpy as np
 import json
 import cv2
-import pandas as pd
-import numpy as np
-import numpy as np
-import matplotlib.pyplot as plt
+from pycocotools.coco import COCO
 
 if not os.path.exists("data"):
     os.mkdir("data")
     os.mkdir("data/train")
     os.mkdir("data/val")
 
+#load train annotations
 with open('data/train/annotations.json') as json_file:
     train_coco_inp= json.load(json_file)
 print("Keys of train annotation file: ",train_coco_inp.keys())
 print("No of Categories(Classes) present: ",len(train_coco_inp['categories']))
 
+#load val annotations
 with open('data/val/annotations.json') as json_file:
     val_coco_inp= json.load(json_file)
+print("Keys of val annotation file: ", val_coco_inp.keys())
+print("No of Categories(Classes) present: ",len(val_coco_inp['categories']))
 
-for i in val_coco_inp["annotations"]:
-    if i["image_id"] == 81931:
-        print(i["category_id"])
-
-#get segmentation points
-seg = val_coco_inp["annotations"][0]["segmentation"]
-seg_chicken = val_coco_inp["annotations"][1]["segmentation"]
-
-''' check which are the categories
-for i in val_coco_inp["categories"]:
-    if i["id"] == 1022:
-        print(i["name"])
-    if i["id"] == 1788:
-        print(i["name"])
-'''
-
-img = cv2.imread('data/val/images/081931.jpg', cv2.COLOR_BGR2RGB)
-
-seg_chicken = seg_chicken[0]
-seg = seg[0]
-
-print(seg_chicken)
-x1 = (559, 185)
-x2 = (429, 164)
-x3 = (372, 255)
-x4 = (443, 342)
-x5 = (479,361)
-x6 = (447,382)
-x7 =(424,422)
-x8 = (405,452)
-
-img = cv2.circle(img, x1, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x2, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x3, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x4, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x5, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x6, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x7, 2, (255, 0, 0), 2)
-img = cv2.circle(img, x8, 2, (255, 0, 0), 2)
-
-y1 = (553, 496)
-y1 = (362, 522)
-y1 = (553, 496)
-y1 = (553, 496)
-y1 = (553, 496)
-
-
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-'''
-useless = []
-for i in train_coco_inp['images']:
-  im = cv2.imread(f"data/train/images/{i['file_name']}")
-  if((im.shape[0]!=i['height']) or (im.shape[1]!=i['width'])):
-    #print(i, im.shape)
-    useless.append(i)
-  #else:
-    #print(".",end="")
-
-print(len(useless))
-
+#remove images with mismatching dimensions
 #There are rotated 27 images, unuseless, so change dataset or remove them (in book shows these images)
 
-'''
+if not os.path.exists("data/train/annotations_correct.json"):
+
+    useless = []
+    for i in train_coco_inp['images']:
+      im = cv2.imread(f"data/train/images/{i['file_name']}")
+      if (im.shape[0] != i['height']) or (im.shape[1] != i['width']):
+        useless.append(i)
+
+    print("Number of images with mismatching dimensions: ", len(useless))
+    bad_ids = [item["id"] for item in useless]
+    for i, item in enumerate(train_coco_inp['images']):
+      if item["id"] in bad_ids:
+        del train_coco_inp["images"][i]
+
+    for i, item in enumerate(train_coco_inp['annotations']):
+      if item["id"] in bad_ids:
+        del train_coco_inp["annotations"][i]
+
+    with open("data/train/annotations_correct.json", "w") as f:
+      f.write(json.dumps(train_coco_inp))
+
+#Loading into memory COCO dataset
+cat_train = COCO("data/train/annotations_correct.json")
+cat_ids = cat_train.getCatIds()
+categories = cat_train.loadCats(cat_ids)
+
+names = [c["name_readable"] for c in categories]
+
+print("Number of different categories: ", len(names))
+
+#checking if there are empty annotations
+count = 0
+for i in train_coco_inp["annotations"]:
+    if not i["segmentation"][0]:
+        count += 1
+print(count)
+
+#checking if there are annotations with missing numbers
+count = 0
+for i in train_coco_inp["annotations"]:
+    if len(i["segmentation"][0])%2 == 1:
+        count += 1
+print(count)
+
+img_width = []
+img_height = []
+
+for item in train_coco_inp["images"]:
+    img_width.append(item["width"])
+    img_height.append(item["height"])
+
+max_width, min_width = max(img_width), min(img_width)
+max_height, min_height = max(img_height), min(img_height)
+
+mean_width = np.mean(img_width)
+mean_height = np.mean(img_height)
+
+print("max_width: ", max_width)
+print("min_width: ", min_width)
+print("mean_width: ", mean_width)
+print("max_height: ", max_height)
+print("min_height: ", min_height)
+print("mean_height: ", mean_height)
+
+#if you want try also mode, anyway probably best size may be 512 as it is the
+#nearest power of two to the mean
+
+
+
+
+
+
+
